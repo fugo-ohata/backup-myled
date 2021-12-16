bda4101b1905e86e9a9181a22c2078ce606f1abd
Fugo Ohata <s20c1026bg@chibakoudai.jp>
2021-12-16 09:17:15 +0000

    first commit

--- myled.c
+++ myled.c
@@ -23,9 +23,14 @@ static ssize_t led_write(struct file*flip, const char* buf, size_t count, loff_t
 //	printk(KERN_INFO "receive %c\n",c);
         if(c == '0'){
 		gpio_base[10] = 1 << 25;
+		gpio_base[10] = 1 << 18;
 	}
-	else if(c == '1')
+	else if(c == '1'){
 		gpio_base[7] = 1 << 25;
+	}
+	else if(c == '2'){
+                gpio_base[7] = 1 << 18;
+	}
 
 	return 1;
 }
@@ -71,11 +76,13 @@ static int __init init_mod(void)
 		
 		gpio_base = ioremap_nocache(0xfe200000, 0xA0);
                 
-		const u32 led = 25;
-		const u32 index = led/10;
-		const u32 shift = (led%10)*3;
-		const u32 mask = ~(0x7 << shift);
+		const u32 led = 25,led1 = 18;
+		const u32 index = led/10, index1 = led1/10;
+		const u32 shift = (led%10)*3, shift1 = (led1%10)*3;
+		const u32 mask = ~(0x7 << shift), mask1 = (0x7 << shift1);
 		gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+		gpio_base[index1] = (gpio_base[index1] & mask1) | (0x1 << shift);
+				
 		return 0;
 }
 
